ccd2da2f4314d699e4624b98e57b66038192a4fa
Anuj Datar <anuj.datar@gmail.com>
2018-10-09 18:54:54 -0400

    set up basic electron app

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from 'express'
-import path from 'path'
-import bodyParser from 'body-parser'
-import os from 'os'
+'use strict'
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const os = require('os')
 
 
 // init express server
@@ -22,21 +23,33 @@ app.use(bodyParser.json())
 
 
 // start server and log server address and port *******************************
-app.listen(3000, () => {
-  const networkInterfaces = os.networkInterfaces()
-  const networkIp = networkInterfaces['Wi-Fi'][1]['address']
-  const localIp = networkInterfaces['Loopback Pseudo-Interface 1'][1]['address']
+const networkInterfaces = os.networkInterfaces()
+const networkIp = networkInterfaces['Wi-Fi'][1]['address']
+const localIp = networkInterfaces['Loopback Pseudo-Interface 1'][1]['address']
+const port = 3000
+const networkAddress = 'http://' +networkIp +':' +port
+const localAddress = 'http://' +localIp +':' +port
+const localAddress2 = 'http://localhost:' +port
+
+app.listen(port, () => {
   console.log('Express.js server running at')
-  console.log('- Local:   http://'+localIp +':3000/ or http://localhost:3000/')
-  console.log('- Network: http://'+networkIp +':3000/')
-  // console.log(os.networkInterfaces())
+  console.log('- Local:   '+localAddress +'  or  ' +localAddress2)
+  console.log('- Network: ' +networkAddress)
 })
 // ****************************************************************************
 
 
 // set up basic request routing ***********************************************
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Electron Express Awesomeness',
+    data: {
+      networkIp: networkAddress,
+      localIp: localAddress,
+      localIp2: localAddress2,
+      test: 'Hannah Bear'
+    }
+  })
 })
 
 app.get('/about', (req, res) => {
