85b7e0cfe293d865cc252a200ec409ad736d7713
Anuj Datar <anuj.datar@gmail.com>
2018-10-09 18:55:16 -0400

    set up basic electron app

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const nodemon = require('nodemon')
-// const server = require('./server')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
