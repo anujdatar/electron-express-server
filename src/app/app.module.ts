6c86c950c076342d273e17871cbfbfc337d2762a
Anuj Datar <anuj.datar@gmail.com>
2018-10-12 07:44:27 -0400

    reset the angular app because of routing issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
-import { ModulesComponent } from './modules/modules.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    AboutComponent,
-    ModulesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
