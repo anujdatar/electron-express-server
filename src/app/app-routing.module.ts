be2a0bed45ec4672d0767a35242f83e834b8e546
Anuj Datar <anuj.datar@gmail.com>
2018-10-29 13:42:23 -0400

    updated routing for components, and app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AddonsComponent } from './addons/addons.component';
+import { SettingsComponent } from './settings/settings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: 'addons',
+    component: AddonsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
