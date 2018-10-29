a7642f7402c8a15a70ee6883e2f6b55be1be77fa
Anuj Datar <anuj.datar@gmail.com>
2018-10-29 12:59:04 -0400

    updated git-ignore/readme, added electron and express js files

---
+++ test_front.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+
+const myServer = express()
+// serve angular front end files from root path
+myServer.use('/', express.static('dist/electron-express-server', { redirect: false }));
+
+// rewrite virtual urls to angular app to enable refreshing of internal pages
+myServer.get('*', function (req, res, next) {
+    res.sendFile(path.resolve('dist/electron-express-server/index.html'));
+});
+
+myServer.listen(4200, () => {
+  console.log('http://localhost:4200')
+})
