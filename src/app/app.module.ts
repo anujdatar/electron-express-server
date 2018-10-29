6b11cbe256dfbe058a4990ca030f56308f527dc9
Anuj Datar <anuj.datar@gmail.com>
2018-10-29 13:38:03 -0400

    added about, addons, dash, nav, settings component scaffoldls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AddonsComponent } from './addons/addons.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AddonsComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
