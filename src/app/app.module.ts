ed2685e15210c607919a9566a112660583442690
Anuj Datar <anuj.datar@gmail.com>
2018-10-30 06:10:18 -0400

    Revert "added about, addons, dash, nav, settings component scaffoldls"
    This reverts commit 6b11cbe256dfbe058a4990ca030f56308f527dc9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { AddonsComponent } from './addons/addons.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    AddonsComponent,
-    DashboardComponent,
-    NavbarComponent,
-    SettingsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
