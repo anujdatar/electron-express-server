7fa0ebcfbadd1cfb4f830deddeed4400c46aea48
Anuj Datar <anuj.datar@gmail.com>
2018-10-30 06:09:56 -0400

    Revert "updated routing for components, and app.component.html"
    This reverts commit be2a0bed45ec4672d0767a35242f83e834b8e546.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { AddonsComponent } from './addons/addons.component';
-import { SettingsComponent } from './settings/settings.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'settings',
-    component: SettingsComponent
-  },
-  {
-    path: 'addons',
-    component: AddonsComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
